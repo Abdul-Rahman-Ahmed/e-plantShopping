29d57bfbe94025fc536d32c482c7d3679eb96798
Abdul-Rahman Ahmed <abdulrahman102034@gmail.com>
2024-10-30 04:00:42 +0300

    last update

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
+    totalQuantity: 0,
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      state.totalQuantity += 1;
+      const existingItem = state.items.find((item) => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.name !== action.payload.name
+      );
+      state.totalQuantity -= action.payload.quantity;
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find((item) => item.name === name);
+      if (itemToUpdate) {
+        state.totalQuantity += quantity - itemToUpdate.quantity;
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
